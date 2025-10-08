e7dc6f752eaecab0e9a79ddbcef20f74d0593d2b
Emmanuel Cousin <emmanuel@hey.com>
2025-10-08 15:49:30 -0300

    add tests for POST admins/links#is_adult

--- spec/controllers/admin/links_controller_spec.rb
+++ spec/controllers/admin/links_controller_spec.rb
@@ -7,4 +7,24 @@ describe Admin::LinksController do
   render_views
 
   it_behaves_like "inherits from Admin::BaseController"
+
+  let(:admin_user) { create(:admin_user) }
+  let(:is_adult) { false }
+  let(:product) { create(:product, is_adult: false) }
+
+  before do
+    sign_in admin_user
+  end
+
+  describe "POST is_adult" do
+    it "updates the product's is_adult flag" do
+      post :is_adult, params: { id: product.unique_permalink, is_adult: "1" }
+      expect(response).to be_successful
+      expect(product.reload.is_adult).to be(true)
+
+      post :is_adult, params: { id: product.unique_permalink, is_adult: "0" }
+      expect(response).to be_successful
+      expect(product.reload.is_adult).to be(false)
+    end
+  end
 end
